0b690446043ec6f5719d9a9fbc017aebc513dfac
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2020-07-02 19:40:49 -0400

    Update model.go

--- API/models/model.go
+++ API/models/model.go
@@ -1,9 +1,11 @@
 package models
 
+import("time")
+
 type Event struct {
   ID             uint   `json:"id" gorm:"primary_key"`
   StationID      string `json:"stationid"`
-  DateTime       string  `json:"datetime"`
+  DateTime       time.Time  `json:"datetime"`
   Temperature    float32 `json:"temperature"`
   Pressure       float32 `json:"pressure"`
   Humidity       float32 `json:"humidity"`
