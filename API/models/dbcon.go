81e3036b74326aa27b3a522e94fa9729e896e536
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2020-07-02 19:40:31 -0400

    Update dbcon.go

--- API/models/dbcon.go
+++ API/models/dbcon.go
@@ -2,13 +2,13 @@ package models
 
 import (
   "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/sqlite"
+  _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-  database, err := gorm.Open("sqlite3", "weather.db")
+  database, err := gorm.Open("mysql", "storm:cloud@(127.0.0.1)/stormcloud?charset=utf8&parseTime=True&loc=Local")
 
   if err != nil {
     panic("Failed to connect to database!")
