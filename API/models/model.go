64d09ebaf09007aa5e015bf3e2722e1de57fb8cb
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2020-07-01 21:47:38 -0400

    Update model.go

--- API/models/model.go
+++ API/models/model.go
@@ -3,12 +3,13 @@ package models
 type Event struct {
   ID             uint   `json:"id" gorm:"primary_key"`
   StationID      string `json:"stationid"`
-  Temperature    string `json:"temperature"`
-  Pressure       string `json:"pressure"`
-  Humidity       string `json:"humidity"`
-  BusVoltage     string `json:"busvoltage"`
-  BusCurrent     string `json:"buscurrent"`
-  SupplyVoltage  string `json:"supplyvoltage"`
-  ShuntVoltage   string `json:"shuntvoltage"`
-  Power          string `json:"power"`
+  DateTime       string  `json:"datetime"`
+  Temperature    float32 `json:"temperature"`
+  Pressure       float32 `json:"pressure"`
+  Humidity       float32 `json:"humidity"`
+  BusVoltage     float32 `json:"busvoltage"`
+  BusCurrent     float32 `json:"buscurrent"`
+  SupplyVoltage  float32 `json:"supplyvoltage"`
+  ShuntVoltage   float32 `json:"shuntvoltage"`
+  Power          float32 `json:"power"`
 }
