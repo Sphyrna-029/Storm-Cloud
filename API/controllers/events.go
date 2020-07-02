97c7f6e6d4bf7c5d1236dd23abbe7301bce17858
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2020-07-02 19:39:36 -0400

    Update events.go

--- API/controllers/events.go
+++ API/controllers/events.go
@@ -3,22 +3,23 @@
 package controllers
 
 import (
-	"net/http"
+        "net/http"
+        "time"
         "github.com/gin-gonic/gin"
         "github.com/BuckarewBanzai/storm-cloud/models"
 )
 
 type CreateEventInput struct {
-	StationID  string `json:"stationid" binding:"required"`
-	DateTime string `json:"datetime" binding:"required"`
-	Temperature float32 `json:"temperature" binding:"required"`
-	Pressure float32 `json:"pressure" binding:"required"`
-	Humidity float32 `json:"humidity" binding:"required"`
-	BusVoltage float32 `json:"busvoltage" binding:"required"`
-	BusCurrent float32 `json:"buscurrent" binding:"required"`
-	SupplyVoltage float32 `json:"supplyvoltage" binding:"required"`
-	ShuntVoltage float32 `json:"shuntvoltage" binding:"required"`
-	Power float32 `json:"power" binding:"required"`
+        StationID  string `json:"stationid" binding:"required"`
+        DateTime time.Time `json:"datetime" binding:"required"`
+        Temperature float32 `json:"temperature" binding:"required"`
+        Pressure float32 `json:"pressure" binding:"required"`
+        Humidity float32 `json:"humidity" binding:"required"`
+        BusVoltage float32 `json:"busvoltage" binding:"required"`
+        BusCurrent float32 `json:"buscurrent" binding:"required"`
+        SupplyVoltage float32 `json:"supplyvoltage" binding:"required"`
+        ShuntVoltage float32 `json:"shuntvoltage" binding:"required"`
+        Power float32 `json:"power" binding:"required"`
 }
 
 // GET /events
@@ -33,40 +34,40 @@ func FindEvents(c *gin.Context) {
 // GET /event/:stationid
 // Find events by stationid
 func FindStation(c *gin.Context) {
-	// Get model if exist
-	var events []models.Event
-	if err := models.DB.Where("station_id = ?", c.Param("id")).Find(&events).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Station ID not found!"})
-		return
-	}
+        // Get model if exist
+        var events []models.Event
+        if err := models.DB.Where("station_id = ?", c.Param("id")).Find(&events).Error; err != nil {
+                c.JSON(http.StatusBadRequest, gin.H{"error": "Station ID not found!"})
+                return
+        }
 
-	c.JSON(http.StatusOK, gin.H{"data": events})
+        c.JSON(http.StatusOK, gin.H{"data": events})
 }
 
 // POST /events
 // Create new weather event
 func CreateEvent(c *gin.Context) {
-	// Validate input
-	var input CreateEventInput
-	if err := c.ShouldBindJSON(&input); err != nil {
-	  c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	  return
-	}
-  
-	// Create event
-	events := models.Event{ 
-		                    StationID: input.StationID, 
-				    DateTime: input.DateTime, 
-				    Temperature: input.Temperature, 
-				    Pressure: input.Pressure, 
-				    Humidity: input.Humidity, 
-				    BusVoltage: input.BusVoltage, 
-				    BusCurrent: input.BusCurrent, 
-				    SupplyVoltage: input.SupplyVoltage, 
-				    ShuntVoltage: input.ShuntVoltage, 
-				    Power: input.Power }
+        // Validate input
+        var input CreateEventInput
+        if err := c.ShouldBindJSON(&input); err != nil {
+          c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+          return
+        }
 
-	models.DB.Create(&events)
-  
-	c.JSON(http.StatusOK, gin.H{"data": events})
+        // Create event
+        events := models.Event{
+                                    StationID: input.StationID,
+                                    DateTime: input.DateTime,
+                                    Temperature: input.Temperature,
+                                    Pressure: input.Pressure,
+                                    Humidity: input.Humidity,
+                                    BusVoltage: input.BusVoltage,
+                                    BusCurrent: input.BusCurrent,
+                                    SupplyVoltage: input.SupplyVoltage,
+                                    ShuntVoltage: input.ShuntVoltage,
+                                    Power: input.Power }
+
+        models.DB.Create(&events)
+
+        c.JSON(http.StatusOK, gin.H{"data": events})
   }
